5c334117224df7d57ffc38e07c639c79653ff250
bt273 <b.temperton@exeter.ac.uk>
2017-11-10 10:25:39 +0000

    Fixed issue with cluster dropdown not having an 'all' option

--- comparisonPanel.R
+++ comparisonPanel.R
@@ -3,9 +3,18 @@ load.comparison.panel<-function(cluster_df){
          
          selectInput(inputId = 'hybrid_rep',
                      label='Cluster Type:',
-                     choices=unique(cluster_df$type)),
+                     choices=c('Choose a type ...' = 0, unique(cluster_df$type))),
          
          DT::dataTableOutput(outputId = 'clusters'),
          
          plotOutput(outputId = 'cluster_summary')))
+}
+
+prepare.cluster.dataframe<-function(input, cluster_df){
+  
+  if(input$hybrid_rep >0){
+    cluster_df<-cluster_df %>% filter(type==input$hybrid_rep)
+  }  
+  
+  return(cluster_df)
 }
